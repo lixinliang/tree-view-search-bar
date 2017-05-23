ef2beb6ff66459e3298bfd99b1da3fbd91f6dbfd
李昕亮 <lixinliang@yy.com>
2017-05-23 09:58:56 +0800

    update
    update

--- lib/views/app.vue
+++ lib/views/app.vue
@@ -8,8 +8,8 @@ import {
     NAME,
 } from '../module/constant';
 
-let slice = ( elements ) => [].slice.call(elements);
 let sleep = ( delay ) => new Promise(( resolve ) => setTimeout(resolve, delay));
+let nextTick = () => new Promise(( resolve ) => process.nextTick(resolve));
 
 export default {
     components : {
@@ -19,59 +19,102 @@ export default {
         height : {
             get () {},
             async set ( value ) {
-                let [{ treeView }] = await requirePackages('tree-view');
-                treeView.element.style['padding-top'] = `${ value }px`;
+                this.$treeView.element.style['padding-top'] = `${ value }px`;
             },
+        },
+        style () {
+            return this.active ? {
+                'z-index' : 3,
+            } : {};
+        },
+        moduleId () {
+            if (this.$el) {
+                for (let attribute of Object.keys(this.$el.dataset)) {
+                    if (/^v-/.test(attribute)) {
+                        return `data-${ attribute }`;
+                    }
+                }
+            }
+            return '';
         },
     },
     data () {
         return {
             NAME,
             tags : [],
-            placeholder : 'type sth',
+            active : false,
+            placeholder : 'Type...',
         };
     },
-    created () {
+    async mounted () {
+        // SetAttribute
+        if (this.moduleId) {
+            this.$treeView.element.setAttribute(this.moduleId, '');
+        }
+        // Bind
         this.$on('focus', () => {
             console.log('focus');
         });
-        this.$on('show', ( resolve ) => {
-            this.height = parseInt(getComputedStyle(this.$refs.tags.$el)['height']);
+        this.$on('show', async ( resolve = () => {} ) => {
+            this.updateView();
+            await this.transitionend();
+            this.active = true;
             resolve();
         });
-        this.$on('hide', async ( resolve ) => {
+        this.$on('hide', async ( resolve = () => {} ) => {
+            this.active = false;
+            await nextTick();
             this.height = 0;
-            await sleep(600);
+            await this.transitionend();
             this.$destroy();
             resolve();
         });
+        // Show
+        await nextTick();
+        this.$emit('show');
     },
-    mounted () {
-        process.nextTick(() => {
-            this.$emit('show', () => {});
-        });
-    },
-    methods : {
-        onChange () {
-            console.log('onChange');
-        },
+    updated () {
+        this.updateView();
     },
     destroyed () {
+        // RemoveAttribute
+        if (this.moduleId) {
+            this.$treeView.element.removeAttribute(this.moduleId);
+        }
+        // Unbind
+        this.$off();
+        // Destroy
+        this.$el.remove();
         if (DEV) {
-            let styles = slice(document.querySelectorAll(`atom-styles style[source-path="${ __filename }"]`));
+            let styles = Array.from(document.querySelectorAll(`atom-styles style[source-path="${ __filename }"]`));
             for (let style of styles) {
                 style.remove();
             }
         }
-        this.$el.remove();
+    },
+    methods : {
+        onChange () {
+            console.log(this.tags);
+        },
+        updateView () {
+            this.height = this.$el.offsetHeight;
+        },
+        transitionend () {
+            return new Promise(( resolve ) => {
+                let handler = () => {
+                    this.$treeView.element.removeEventListener('transitionend', handler, false);
+                    resolve();
+                };
+                this.$treeView.element.addEventListener('transitionend', handler, false);
+            });
+        },
     },
 }
 </script>
 
 <template>
-    <div class="app" :name="NAME.kebab">
+    <div class="app" :name="NAME.kebab" :style="style">
         <input-tag
-            ref="tags"
             class="tags"
             :tags="tags"
             :on-change="onChange"
@@ -88,15 +131,12 @@ export default {
         width: 100%;
         padding: 0 .4em;
         box-sizing: border-box;
+        & + .tree-view-resizer {
+            transition: padding .4s;
+        }
     }
     .tags {
         border: none;
         background-color: transparent;
-    }
-</style>
-
-<style lang="less">
-    .app + .tree-view-resizer {
-        transition: padding .4s;
     }
 </style>
